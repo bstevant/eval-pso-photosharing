033e00f87c66422f0011042a46bdcbf4d1200f55
Bruno STEVANT <bruno.stevant@gmail.com>
2020-07-14 23:31:19 +0200

    Edgeinfra: add acceleration factor + values

--- infra/edgeinfra.py
+++ infra/edgeinfra.py
@@ -8,35 +8,35 @@ ui_get_minrtt = { "dsl_dsl":4000, "dsl_fib":1000,
                  "fib_dsl":4000, "fib_fib":200}
 
 ui_get_medrtt = { "dsl_dsl":8000, "dsl_fib":4000,
-                 "fib_dsl":8000, "fib_fib":400}
+                 "fib_dsl":8000, "fib_fib":500}
 
 # call1: post photo to UI
 ui_post_minrtt = { "dsl_dsl":20000, "dsl_fib":20000,
                  "fib_dsl":5000, "fib_fib":500}
 
 ui_post_medrtt = { "dsl_dsl":40000, "dsl_fib":40000,
-                 "fib_dsl":20000, "fib_fib":1500}
+                 "fib_dsl":20000, "fib_fib":2000}
 
 # call1: get list from MH
-mh_get_minrtt = { "dsl_dsl":130, "dsl_fib":130,
-                 "fib_dsl":100, "fib_fib":90}
+mh_get_minrtt = { "dsl_dsl":130, "dsl_fib":110,
+                 "fib_dsl":110, "fib_fib":90}
 
-mh_get_medrtt = { "dsl_dsl":170, "dsl_fib":170,
-                 "fib_dsl":140, "fib_fib":100}
+mh_get_medrtt = { "dsl_dsl":180, "dsl_fib":150,
+                 "fib_dsl":150, "fib_fib":100}
 
 # call2: get photo from PH
 ph_get_minrtt = { "dsl_dsl":20000, "dsl_fib":5000,
                  "fib_dsl":20000, "fib_fib":500}
 
 ph_get_medrtt = { "dsl_dsl":40000, "dsl_fib":20000,
-                 "fib_dsl":40000, "fib_fib":1500}
+                 "fib_dsl":40000, "fib_fib":2000}
 
 # call3: put photo to PH
 ph_post_minrtt = { "dsl_dsl":20000, "dsl_fib":20000,
                  "fib_dsl":5000, "fib_fib":500}
 
 ph_post_medrtt = { "dsl_dsl":40000, "dsl_fib":40000,
-                 "fib_dsl":20000, "fib_fib":1500}
+                 "fib_dsl":20000, "fib_fib":2000}
 
 # call4: get thumb from TH
 th_get_minrtt = { "dsl_dsl":1400, "dsl_fib":800,
@@ -45,6 +45,12 @@ th_get_minrtt = { "dsl_dsl":1400, "dsl_fib":800,
 th_get_medrtt = { "dsl_dsl":2200, "dsl_fib":1400,
                  "fib_dsl":2200, "fib_fib":650}
 
+# call4: get thumb from TH (a=2)
+th_get2_minrtt = { "dsl_dsl":1000, "dsl_fib":500,
+                 "fib_dsl":1000, "fib_fib":300}
+
+th_get2_medrtt = { "dsl_dsl":2000, "dsl_fib":1000,
+                 "fib_dsl":2000, "fib_fib":400}
 
 class Node:
     nb_nodes = 0
@@ -126,6 +132,7 @@ class Infra:
     def __init__(self, n_dsl, n_fiber, method="uni", file=None):
         self.nodes = []
         self.clients = []
+        self.a2 = []
         self.ui_get_matrix  = []
         self.ui_post_matrix = []
         self.ph_get_matrix  = []
@@ -146,6 +153,8 @@ class Infra:
             
         # 20% of nodes are clients
         self.clients = random.sample(range(0,len(self.nodes)), int(len(self.nodes)/5))
+        # 20% of nodes have a_factor = 2
+        self.a2 = random.sample(self.nodes, int(len(self.nodes)/5))
         
         self.init_matrix(self.ui_get_matrix,  ui_get_minrtt, ui_get_medrtt, method)
         self.init_matrix(self.ui_post_matrix, ui_post_minrtt, ui_post_medrtt, method)
@@ -153,6 +162,7 @@ class Infra:
         self.init_matrix(self.ph_post_matrix, ph_post_minrtt, ph_post_medrtt, method)
         self.init_matrix(self.mh_get_matrix,  mh_get_minrtt, mh_get_medrtt, method)
         self.init_matrix(self.th_get_matrix,  th_get_minrtt, th_get_medrtt, method)
+        self.updt_matrix(self.th_get_matrix,  self.a2, th2_get_minrtt, th2_get_medrtt, method)
     
     def init_matrix(self, matrix, minrtt, medrtt, method="uni"):
         m = {}
@@ -176,6 +186,28 @@ class Infra:
                 line.append(int(l))
             matrix.append(line)
 
+    def updt_matrix(self, matrix, nodes, minrtt, medrtt, method="uni"):
+        m = {}
+        if method == "pareto":
+            m["dsl_dsl"] = self.gen_pareto(minrtt["dsl_dsl"], medrtt["dsl_dsl"], len(self.nodes)**2)
+            m["dsl_fib"] = self.gen_pareto(minrtt["dsl_fib"], medrtt["dsl_fib"], len(self.nodes)**2)
+            m["fib_dsl"] = self.gen_pareto(minrtt["fib_dsl"], medrtt["fib_dsl"], len(self.nodes)**2)
+            m["fib_fib"] = self.gen_pareto(minrtt["fib_fib"], medrtt["fib_fib"], len(self.nodes)**2)
+        for i in self.nodes:
+            line = []
+            for j in nodes:
+                link_type = i.node_type + "_" + j.node_type
+                if method == "pareto":
+                    v = m[link_type]
+                else:
+                    v = medrtt[link_type]
+                l = Link(v, method)
+                # Define low rtt for self-calling
+                if (i == j):
+                    l.rtt = l.rtt / 10
+                line.append(int(l))
+            matrix[i] = line
+    
     def serial_matrix(self, matrix):
         m = []
         for line in matrix:
